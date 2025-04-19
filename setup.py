c5f71bd6cbf4de8d929c0bb9b665957a8adbf672
Movya <movya.ms@gmail.com>
2025-04-18 21:14:48 -0400

    Data Transformation

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from setuptools import find_packages, setup 
 from typing import List
 
-HYPEN_E_DOT = '-e .'
-
 def get_requirements(file_path: str) -> List[str]:
     """
     Reads requirements.txt and returns a clean list of packages,
