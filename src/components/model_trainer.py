c7fc8f79384f260ae450bd660323433e30fba965
Movya <movya.ms@gmail.com>
2025-04-19 20:15:36 -0400

    model trainer function

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -18,3 +18,17 @@ from src.exception import CustomException
 from src.logger import logging
 
 from src.utils import save_object
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+
+    def initiate_model_trainer(self,train_array,test_array,preprocessor_path):
+        try:
+            logging.info("Split train and test input data")
+        except:
+            pass
